d928a1bb2d47c20d894e2972680a552138b442af
meet khanpara <meetkhanpara095@gmail.com>
2025-06-17 10:37:12 +0530

    18: meeting data table

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -1,11 +1,8 @@
 import { db } from "@/db";
-import { meetings } from "@/db/schema";
-import {
-  createTRPCRouter,
-  protectedProcedure,
-} from "@/trpc/init";
+import { agents, meetings } from "@/db/schema";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { z } from "zod";
-import { and, count, desc, eq, getTableColumns, ilike } from "drizzle-orm";
+import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 import {
   DEFAULT_PAGE,
   DEFAULT_PAGE_SIZE,
@@ -54,7 +51,9 @@ export const meetingsRouter = createTRPCRouter({
           ...getTableColumns(meetings),
         })
         .from(meetings)
-        .where(and(eq(meetings.id, input.id) , eq(meetings.userId, ctx.auth.user.id)));
+        .where(
+          and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id))
+        );
       if (!existingMeeting) {
         throw new TRPCError({
           code: "NOT_FOUND",
@@ -66,49 +65,48 @@ export const meetingsRouter = createTRPCRouter({
     }),
   getMany: protectedProcedure
     .input(
-      z
-        .object({
-          page: z.number().default(DEFAULT_PAGE),
-          pageSize: z
-            .number()
-            .min(MIN_PAGE_SIZE)
-            .max(MAX_PAGE_SIZE)
-            .default(DEFAULT_PAGE_SIZE),
-          search: z.string().nullish(),
-        })
+      z.object({
+        page: z.number().default(DEFAULT_PAGE),
+        pageSize: z
+          .number()
+          .min(MIN_PAGE_SIZE)
+          .max(MAX_PAGE_SIZE)
+          .default(DEFAULT_PAGE_SIZE),
+        search: z.string().nullish(),
+      })
     )
-    .query(async ({ ctx, input}) => {
+    .query(async ({ ctx, input }) => {
       const { search, page, pageSize } = input;
       const data = await db
         .select({
           ...getTableColumns(meetings),
+          agent: agents,
+          duration: sql<number>`EXTRACT(EPOCH FROM (${meetings.endedAt} - ${meetings.startedAt}))`.as("duration"),
         })
         .from(meetings)
+        .innerJoin(agents, eq(meetings.agentId, agents.id))
         .where(
           and(
             eq(meetings.userId, ctx.auth.user.id),
-            search
-              ? ilike(meetings.name, `%${search}%`)
-              : undefined,
+            search ? ilike(meetings.name, `%${search}%`) : undefined
           )
         )
-        .orderBy(desc(meetings.createdAt),desc(meetings.id))
+        .orderBy(desc(meetings.createdAt), desc(meetings.id))
         .limit(pageSize)
         .offset((page - 1) * pageSize);
 
-        const [total] = await db
-        .select({count : count()})
+      const [total] = await db
+        .select({ count: count() })
         .from(meetings)
+        .innerJoin(agents, eq(meetings.agentId, agents.id))
         .where(
           and(
             eq(meetings.userId, ctx.auth.user.id),
-            search
-              ? ilike(meetings.name, `%${search}%`)
-              : undefined,
+            search ? ilike(meetings.name, `%${search}%`) : undefined
           )
         );
 
-        const totalPages = Math.ceil(total.count / pageSize);
+      const totalPages = Math.ceil(total.count / pageSize);
       // await new Promise(resolve => setTimeout(resolve, 5000));
       return {
         items: data,
