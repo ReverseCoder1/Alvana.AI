20c6a2dc57371c6b4bb32ca00c18761107841f45
meet khanpara <meetkhanpara095@gmail.com>
2025-06-29 17:54:05 +0530

    27: Payments

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,26 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db"; 
-
+import { polar, checkout, portal} from "@polar-sh/better-auth";
 import * as schema from "@/db/schema";
-
+import { polarClient } from "./polar";
 export const auth = betterAuth({
     allowedOrigins: [
         "http://localhost:3000", // local dev
         "https://moved-widely-firefly.ngrok-free.app" // your ngrok domain
+    ],
+    plugins: [
+        polar({
+            client: polarClient, // Polar client instance
+            createCustomerOnSignUp: true, // Automatically create a Polar customer on sign up
+            use: [
+                checkout({
+                    authenticatedUsersOnly: true,
+                    successUrl: "/upgrade",
+                }),
+                portal(),
+            ],
+        })
     ],
     socialProviders: {
         github: { 
